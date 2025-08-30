9fccbbbf27c267e8e61645e403f8ba57e39a492e
Alexandru Gojinetchi <MrFail@users.noreply.github.com>
2025-08-31 01:33:53 +0300

    refactor Env facade with improved method annotations and typed accessor

--- src/Facades/Env.php
+++ src/Facades/Env.php
@@ -3,10 +3,17 @@
 namespace SoulDoit\SetEnv\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\HigherOrderWhenProxy;
+use Illuminate\Support\Stringable;
 
+/**
+ * @method static void set(string $key, string|bool $value, ?string $comments = null, ?string $afterKey = null)
+ * @method static Stringable|HigherOrderWhenProxy get(string $key)
+ * @method static \SoulDoit\SetEnv\Env envFile(string $env_file)
+ */
 class Env extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'souldoit-set-env';
     }
